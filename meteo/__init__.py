1b2065c2f6ae111f20c66eda9345f797880a9ce0
Namita <54566615+namitasharma01@users.noreply.github.com>
2021-06-30 17:58:53 +0200

    Update __init__.py

--- meteo/__init__.py
+++ meteo/__init__.py
@@ -2,7 +2,7 @@
 # Filename: __init__.py
 from numpy import log10, log
 
-from humidity import *
-from radiation import *
-from air import *
-from physdyn import *
+from meteo.humidity import *
+from meteo.radiation import *
+from meteo.air import *
+from meteo.physdyn import *
